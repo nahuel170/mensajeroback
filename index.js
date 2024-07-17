2affc8c827323e2a0126492c31196ececf735de9
nahuel170 <nahuelraspo20@gmail.com>
2024-07-17 18:04:39 -0300

    Agregar configuración de conexión a MongoDB Atlas

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const corsOptions = {
     allowedHeaders: ['Content-Type', 'Authorization']}
 app.use(cors())
 
-
+conexion();
 const server = http.createServer(app)
 
 const io = new Server(server, {
